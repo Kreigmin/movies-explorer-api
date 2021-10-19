a658d80b7c536bce99284b799959ae9dd01b7efd
Dmitriy Sobolev <leejjuk@gmail.com>
2021-10-19 12:46:10 +0300

    feat: create and configure a project, add all routes, middlewares, central error handler, limiter

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const { celebrate, Joi, errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
+const userRoutes = require('./routes/users');
+const movieRoutes = require('./routes/movies');
+const { createUser, signIn, signOut } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const NotFoundError = require('./errors/not-found-error');
+
+const BASE_ERROR_CODE = 500;
+const { PORT = 3000 } = process.env;
+
+const limiter = rateLimit({
+  windowsMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+const app = express();
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(requestLogger);
+
+app.use(limiter);
+
+app.post('/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(8).required(),
+    }),
+  }),
+  createUser);
+
+app.post('/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().min(8).required(),
+    }),
+  }),
+  signIn);
+
+app.post('/signout', signOut);
+
+app.use(auth);
+
+app.use(userRoutes);
+app.use(movieRoutes);
+
+app.use((req, res, next) => {
+  next(new NotFoundError('Запрашиваемый ресурс не найден'));
+});
+
+app.use(errorLogger);
+
+app.use(errors());
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const { statusCode = BASE_ERROR_CODE, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === BASE_ERROR_CODE ? 'На сервере произошла ошибка' : message,
+  });
+});
+
+app.listen(PORT);
